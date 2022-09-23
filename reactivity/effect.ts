f49a6fdd92826cb8897f12b3f6c573e3fccd5df4
01super <fk1024@outlook.com>
2022-09-23 15:37:59 +0800

    feat：完成reactive基本功能

--- reactivity/effect.ts
+++ reactivity/effect.ts
@@ -1,3 +1,4 @@
+let activeEffect;
 class ReactiveEffect {
   private _fn;
   constructor(fn) {
@@ -8,20 +9,30 @@ class ReactiveEffect {
     this._fn();
   }
 }
+// target => depsMap
 const targetMap = new Map();
-let activeEffect;
 
 export function track(target, key) {
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
-    depsMap.set(target, depsMap);
+    targetMap.set(target, depsMap);
   }
+  // key => effectFn
   let dep = depsMap.get(key);
   if (!dep) {
     dep = new Set();
+    depsMap.set(key, dep);
   }
   dep.add(activeEffect);
+}
+
+export function trigger(target, key, value) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+  for (let effect of dep) {
+    effect.run();
+  }
 }
 
 export function effect(effectFn) {
--- reactivity/reactive.ts
+++ reactivity/reactive.ts
@@ -1,14 +1,18 @@
-import { track } from "./effect";
+import { track, trigger } from "./effect";
 
 export function reactive(row) {
   return new Proxy(row, {
     get: (target, key, receiver) => {
       // 收集依赖
+      // TODO 已经收集依赖的时候
       track(target, key);
       return Reflect.get(target, key, receiver);
     },
-    set: (target, key, newValue, receiver) => {
-      return Reflect.set(target, key, newValue, receiver);
+    set: (target, key, value, receiver) => {
+      const result = Reflect.set(target, key, value, receiver);
+      // 触发依赖
+      trigger(target, key, value);
+      return result;
     },
   });
 }
