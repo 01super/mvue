e1726f4e01c5866653bb2809ed9fd85b0300eeb4
01super <fk1024@outlook.com>
2022-11-07 23:20:21 +0800

    feat: isProxy

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,4 +1,4 @@
-import { isReadonly, readonly } from "../reactive";
+import { isProxy, isReadonly, readonly } from "../reactive";
 
 describe("readonly", () => {
   it("happy path", () => {
@@ -6,6 +6,7 @@ describe("readonly", () => {
     const wrapped = readonly(original);
     expect(wrapped).not.toBe(original);
     expect(isReadonly(wrapped.bar)).toBe(true);
+    expect(isProxy(wrapped)).toBe(true);
     expect(isReadonly(original.bar)).toBe(false);
     expect(wrapped.foo).toBe(1);
   });
